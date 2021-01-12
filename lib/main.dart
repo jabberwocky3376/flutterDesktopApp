4c067a5eb411aae0f2655489286d7eb0045fa75c
akutagawa.junki <akutagawa.junki@ditgroup.jp>
2021-01-12 17:05:41 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'header.dart';
+
+void main() {
+ runApp(App());
+}
+
+class App extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+
+    debugShowCheckedModeBanner: false,
+
+     home: Scaffold(
+
+       appBar: Header(),
+
+       body: Center(child: Text("testtest")),
+
+
+     ),
+   );
+ }
+}
